1fa9823295f33e28782278ecadd1e27030defa05
ItsLJcool <54383469+ItsLJcool@users.noreply.github.com>
2025-01-05 12:52:27 -0700

    reformatted the `WebSocketPacket` to use `Serializer` instead of a long string.
    Also added Meta Data to the packet (can be disabled as a 3rd param)

--- source/funkin/backend/internet/WebSocketPacket.hx
+++ source/funkin/backend/internet/WebSocketPacket.hx
@@ -1,49 +1,27 @@
 package funkin.backend.internet;
 
-import String;
+import funkin.backend.assets.ModsFolder;
+import funkin.backend.system.macros.GitCommitMacro;
+
+import haxe.Serializer;
+
+import Date;
 import StringBuf;
+import String;
 import Reflect;
 
 /**
 * A data object that can be customized for `WebSocketUtil` to send data to the server.
-* Srt was the person that made the data idea with first version of Codename Engine Multiplayer Test.
+* You will need to handle the custom packet on your server yourself.
 *
-* Here is an example of how the default packet looks like:
-* `<PACKET::onReady::key=>value::ENDPACKET>`
+* GENERAL WEBSOCKET WARNING: Sending data to the server on `update` SHOULD NEVER BE DONE!!
+* It will be slow and generally not a good idea. It might overload the server and cause unforseen issues.
 *
-* On the server side its considered as a string so you can use splits to get the data.
+* Why use a packet class instead of sending your own data? Well this Serializes the data and handles it for you, so all you do is just send the class in the `WebSocketUtil.send` and thats it.
 **/
 class WebSocketPacket {
-
 	/**
-	* The starting prefix of the packet.
-	**/
-	static var startPrefix(default, set):String = "<PACKET";
-	private static function set_startPrefix(value:String):String { return value.trim(); }
-	/**
-	* The ending prefix of the packet.
-	**/
-	static var endPrefix(default, set):String = "ENDPACKET>";
-	private static function set_endPrefix(value:String):String { return value.trim(); }
-
-	/**
-	* The pointer to the value.
-	* Default example:
-	* `key=>value`
-	**/
-	static var dataPointer(default, set):String = "=>";
-	private static function set_dataPointer(value:String):String { return value.trim(); }
-	
-	/**
-	* Splits the packet from key / values.
-	* Default example:
-	* `key=>value::key2=>value2`
-	**/
-	static var dataSplit(default, set):String = "::";
-	private static function set_dataSplit(value:String):String { return value.trim(); }
-
-	/**
-	* Just normal json data that is being held for the packet to stringify.
+	* Just normal data that is being held for the packet to get stringified.
 	**/
 	private var packetData:Dynamic = {};
 	
@@ -51,17 +29,41 @@ class WebSocketPacket {
 	* The name of the event the server handles.
 	* If null it won't be added in the packet.
 	**/
-	public var packetEventName:String;
+	public var packetEventName(default, set):String;
+	private function set_packetEventName(value:String):String {
+		if (value == null) return "";
+		return this.packetEventName = value;
+	}
+
+	@:dox(hide) private var add_meta_data:Bool = true;
 
 	/**
 	* @param packetName The name of the event the server handles.
 	* @param packetData The data that is being sent to the server. Can also be a stringified JSON.
+	* @param add_meta_data If true, adds metadata to the packet. This is useful for data like the time it was sent, 
 	**/
-	public function new(packetName:Null<String>, packetData:Dynamic) {
-		this.packetEventName = (packetName == null) ? "" : packetName; 
+	public function new(packetName:Null<String>, packetData:Dynamic, ?_add_meta_data:Bool = true) {
+		this.packetEventName = packetName;
+		this.add_meta_data = _add_meta_data;
 
-		if (packetData is String) packetData = haxe.Json.parse(packetData);
+		// in case ig
+		try {
+			if (packetData is String) packetData = haxe.Json.parse(packetData);
+		} catch (e:Dynamic) {
+			trace("Error parsing string data to packet: " + e);
+		}
+		
 		this.packetData = packetData;
+		
+		if (this.add_meta_data) {
+			try {
+				if (ModsFolder.currentModFolder != null) this.packetData.__mod = ModsFolder.currentModFolder;
+				this.packetData.__commitHash = GitCommitMacro.commitHash; // for checking outdated action builds on the server. its gonna be peak trust.
+			} catch (e:Dynamic) {
+				trace("Error adding metadata to packet: " + e);
+			}
+		}
+
 	}
 
 	/**
@@ -94,14 +96,25 @@ class WebSocketPacket {
 	}
 
 	/**
-	* Converts the packet to a string.
+	* Converts the packet to a string. Uses `Serializer` to convert the packet to a data string
 	* @return The packet as a string.
 	**/
 	public function toString():String {
-		var data:StringBuf = new StringBuf();
-		if (WebSocketPacket.startPrefix != "" && this.packetEventName != "") data.add('${WebSocketPacket.startPrefix}${WebSocketPacket.dataSplit}${this.packetEventName}');
-		for (field in Reflect.fields(this.packetData)) data.add('${WebSocketPacket.dataSplit}${field}${WebSocketPacket.dataPointer}${Reflect.getProperty(this.packetData, field)}');
-		if (WebSocketPacket.endPrefix != "") data.add('${WebSocketPacket.dataSplit}${WebSocketPacket.endPrefix}');
-		return data.toString();
+		var cerial = new Serializer();
+		var buffer = new StringBuf();
+
+		// if no name is associated with packet, just serialize the data
+		if (packetEventName != "") {
+			buffer.add('!HXP');
+			buffer.add(this.packetEventName);
+		} else {
+			buffer.add('!HXp');
+		}
+		buffer.add('=>');
+
+		if (add_meta_data) this.packetData.__timestamp = Date.now().getTime();
+
+		cerial.serialize(this.packetData);
+		return '${buffer.toString()}${cerial.toString()}';
 	}
 }
