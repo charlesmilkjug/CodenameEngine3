204a4ae14163ed7f5e7dc21f9ae4c9b337286494
Ralty <78720179+Raltyro@users.noreply.github.com>
2025-01-29 12:47:51 +0700

    Update DIsplayObjectRenderer to use BitmapUtil
    This will allocate only Hardware BitmapData instead of both software and hardware.
    And this will fix the resizing bitmapdata so ShaderResizeFix are not needed anymore.

--- source/funkin/backend/system/Main.hx
+++ source/funkin/backend/system/Main.hx
@@ -136,7 +136,6 @@ class Main extends Sprite
 		#if UPDATE_CHECKING
 		funkin.backend.system.updating.UpdateUtil.init();
 		#end
-		ShaderResizeFix.init();
 		Logs.init();
 		Paths.init();
 		#if GLOBAL_SCRIPT
